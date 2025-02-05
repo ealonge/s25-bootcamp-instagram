dfb823d30b7e998c5a7ca1ff098ea98b543aef45
eoalonge <eoalonge@unc.edu>
2025-02-04 22:26:04 -0500

    iOS Bootcamp Instagram UI Recreation

---
+++ iOS Bootcamp Instagram Demo/iOS Bootcamp Instagram Demo/iOS_Bootcamp_Instagram_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Bootcamp_Instagram_DemoApp.swift
+//  iOS Bootcamp Instagram Demo
+//
+//  Created by Emmanuel Alonge on 2/4/25.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Bootcamp_Instagram_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
